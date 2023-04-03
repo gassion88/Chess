4ff277082c533de06ff1d20a59e7648888b8f023
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-03 19:01:33 +0300

    gitignore:

--- src/main/java/com/gassion/Board.java
+++ src/main/java/com/gassion/Board.java
@@ -1,16 +1,18 @@
 package com.gassion;
 
+import com.gassion.piece.Pawn;
+
 import java.util.HashMap;
 
 public class Board {
     HashMap<Coordinates, Piece> pieces = new HashMap<>();
 
-    public void setPieces(Piece piece, Coordinates coordinates) {
+    public void setPieces(Coordinates coordinates, Piece piece) {
         piece.coordinates = coordinates;
         pieces.put(coordinates, piece);
     }
 
     public void setupDefaultPeacesPositions() {
-        
+
     }
 }
