2a1e1b3af0bd9ce83172572a2824837f31257f70
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-03 15:04:28 +0300

    fix : add constructor the piece class

--- src/main/java/com/gassion/Piece.java
+++ src/main/java/com/gassion/Piece.java
@@ -3,4 +3,9 @@ package com.gassion;
 abstract public class Piece {
     Color color;
     Coordinates coordinates;
+
+    public Piece(Color color, Coordinates coordinates) {
+        this.color = color;
+        this.coordinates = coordinates;
+    }
 }
