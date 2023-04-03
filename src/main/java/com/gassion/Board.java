6d48aea0458b90410a48c4cd62be4db97bf70b1c
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-03 15:00:08 +0300

    feat : add set peaces function

--- src/main/java/com/gassion/Board.java
+++ src/main/java/com/gassion/Board.java
@@ -4,4 +4,13 @@ import java.util.HashMap;
 
 public class Board {
     HashMap<Coordinates, Piece> pieces = new HashMap<>();
+
+    public void setPieces(Piece piece, Coordinates coordinates) {
+        piece.coordinates = coordinates;
+        pieces.put(coordinates, piece);
+    }
+
+    public void setupDefaultPeacesPositions() {
+        
+    }
 }
