c568409934ad94d94231d3c02cb1d93cc6f52aee
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-04 19:33:17 +0300

    feat : add avaible coordinates set

--- src/main/java/com/gassion/Coordinates.java
+++ src/main/java/com/gassion/Coordinates.java
@@ -1,5 +1,7 @@
 package com.gassion;
 
+import com.gassion.piece.CoordinatesShift;
+
 public class Coordinates {
     public final File file;
     public final Integer rank;
@@ -7,6 +9,20 @@ public class Coordinates {
     public Coordinates(File file, Integer rank) {
         this.file = file;
         this.rank = rank;
+    }
+
+    public Coordinates shift(CoordinatesShift shift) {
+        return new Coordinates(File.values()[this.file.ordinal() + shift.fileShift], this.rank + shift.rankShift);
+    }
+
+    public Boolean canShift(CoordinatesShift shift) {
+        int newFile = this.file.ordinal() + shift.fileShift;
+        int newRank = this.rank + shift.rankShift;
+
+        if (newFile < 0 || newFile > 8) return false;
+        if (newRank < 0 || newRank > 8) return false;
+
+        return true;
     }
 
     @Override
