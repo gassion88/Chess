a18d41255e6c447de1d066acc3173b42e1bed560
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-06 17:39:12 +0300

    fix : private method

--- src/main/java/com/gassion/Game.java
+++ src/main/java/com/gassion/Game.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class Game {
     private final Board board;
-    private BoardConsoleRenderer boardConsoleRenderer = new BoardConsoleRenderer();
+    private final BoardConsoleRenderer boardConsoleRenderer = new BoardConsoleRenderer();
 
     public Game(Board board) {
         this.board = board;
