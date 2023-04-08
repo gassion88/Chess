8fb659ca87c4eaa6d2816f366ce55678e18c8cd2
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-08 23:16:35 +0300

    feat : add movement logic for the rook

--- src/main/java/com/gassion/Main.java
+++ src/main/java/com/gassion/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 //        Board board = new Board();
 //        board.setupDefaultPeacesPositions();
-        Board board = new BoardFactory().fromFen("8/8/1N3n2/8/3B4/8/8/8 - w");
+        Board board = new BoardFactory().fromFen("8/4p3/8/5N1R/8/8/7p/8 - w");
  //      new BoardConsoleRenderer().render(board);
 //
 //        Piece piece = board.getPiece(new Coordinates( File.B, 1));
--- src/main/java/com/gassion/piece/Rook.java
+++ src/main/java/com/gassion/piece/Rook.java
@@ -1,8 +1,12 @@
 package com.gassion.piece;
 
+import com.gassion.Board;
+import com.gassion.BoardUtils;
 import com.gassion.Color;
 import com.gassion.Coordinates;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Rook extends Piece {
@@ -12,6 +16,44 @@ public class Rook extends Piece {
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return null;
+        Set<CoordinatesShift> result = new HashSet<>();
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i, 0));
+        }
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(0, i));
+        }
+
+        return result;
+    }
+
+    @Override
+    protected boolean isSquareAvailableForMove(Coordinates coordinates, Board board) {
+        boolean result = super.isSquareAvailableForMove(coordinates, board);
+
+        if (result) {
+            List<Coordinates> coordinatesBetween;
+
+            if (this.coordinates.file == coordinates.file) {
+                coordinatesBetween = BoardUtils.getVerticalCoordinatesBetween(this.coordinates, coordinates);
+            } else {
+                coordinatesBetween = BoardUtils.getHorizontalCoordinatesBetween(this.coordinates, coordinates);
+            }
+
+            for (Coordinates c : coordinatesBetween) {
+                if (!board.isSquareEmpty(c)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
