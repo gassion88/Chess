e613ba3ecb49394c2fde26c5a9a3696ac87e63fd
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-08 21:04:33 +0300

    feat : add bishop movs

--- src/main/java/com/gassion/Main.java
+++ src/main/java/com/gassion/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args) {
 //        Board board = new Board();
 //        board.setupDefaultPeacesPositions();
-        Board board = new BoardFactory().fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-       new BoardConsoleRenderer().render(board);
+        Board board = new BoardFactory().fromFen("8/8/1N3n2/8/3B4/8/8/8 - w");
+ //      new BoardConsoleRenderer().render(board);
 //
 //        Piece piece = board.getPiece(new Coordinates( File.B, 1));
 //        Set<Coordinates> coordinates = piece.getAvailableMoveSquares(board);
--- src/main/java/com/gassion/piece/Bishop.java
+++ src/main/java/com/gassion/piece/Bishop.java
@@ -1,8 +1,12 @@
 package com.gassion.piece;
 
+import com.gassion.Board;
+import com.gassion.BoardUtils;
 import com.gassion.Color;
 import com.gassion.Coordinates;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Bishop extends Piece {
@@ -12,6 +16,39 @@ public class Bishop extends Piece {
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return null;
+        Set<CoordinatesShift> result = new HashSet<>();
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i, i));
+        }
+
+        for (int i = -7; i <= 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i, -i));
+        }
+
+        return result;
+    }
+
+    @Override
+    protected boolean isSquareAvailableForMove(Coordinates coordinates, Board board) {
+        boolean result = super.isSquareAvailableForMove(coordinates, board);
+
+        if (result) {
+            List<Coordinates> coordinatesBetween = BoardUtils.getDiagonalCoordinatesBetween(this.coordinates, coordinates);
+
+            for (Coordinates c : coordinatesBetween) {
+                if (!board.isSquareEmpty(c)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
