e20d07a8de1971a5f971acd5678ba22dea55e21e
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-10 22:33:50 +0300

    fix : Pawn move bag fixed

--- src/main/java/com/gassion/piece/Pawn.java
+++ src/main/java/com/gassion/piece/Pawn.java
@@ -3,8 +3,10 @@ package com.gassion.piece;
 import com.gassion.board.Board;
 import com.gassion.Color;
 import com.gassion.Coordinates;
+import com.gassion.board.BoardUtils;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Pawn extends Piece {
@@ -39,19 +41,25 @@ public class Pawn extends Piece {
         return result;
     }
 
-    @Override
     protected boolean isSquareAvailableForMove(Coordinates coordinates, Board board) {
         if (this.coordinates.file == coordinates.file) {
-            return board.isSquareEmpty(coordinates);
+            int rankShift = Math.abs(this.coordinates.rank - coordinates.rank);
+
+            if (rankShift == 2) {
+                List<Coordinates> between = BoardUtils.getVerticalCoordinatesBetween(this.coordinates, coordinates);
+
+                return (board.isSquareEmpty(between.get(0))) && board.isSquareEmpty(coordinates);
+            } else {
+                return board.isSquareEmpty(coordinates);
+            }
         } else {
             if (board.isSquareEmpty(coordinates)) {
                 return false;
             } else {
-                return board.getPiece(coordinates).color != this.color;
+                return board.getPiece(coordinates).color != color;
             }
         }
     }
-
     @Override
     protected Set<CoordinatesShift> getPieceAttacks() {
         Set<CoordinatesShift> result = new HashSet<>();
