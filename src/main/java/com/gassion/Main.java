9360c1507d818284bc36edbda95e9652aa09163b
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-12 20:11:44 +0300

    feat: add start position fen

--- src/main/java/com/gassion/Main.java
+++ src/main/java/com/gassion/Main.java
@@ -5,16 +5,9 @@ import com.gassion.board.BoardFactory;
 
 public class Main {
     public static void main(String[] args) {
-//        Board board = new Board();
-//        board.setupDefaultPeacesPositions();
-        Board board = new BoardFactory().fromFen("k2r4/8/8/2P1P3/2PKP3/2PPP3/8/8 - w");
- //      new BoardConsoleRenderer().render(board);
-//
-//        Piece piece = board.getPiece(new Coordinates( File.B, 1));
-//        Set<Coordinates> coordinates = piece.getAvailableMoveSquares(board);
+        Board board = new BoardFactory().fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 
         Game game = new Game(board);
         game.gameLoop();
-
     }
 }
